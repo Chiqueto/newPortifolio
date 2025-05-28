1c975eef08760f301302c44c3e0bba2543b28bd9
Chiqueto <lfchiqueto@gmail.com>
2025-05-27 22:10:19 -0300

    feat: updating project card

--- portifolio-chiqueto/components/project-card.tsx
+++ portifolio-chiqueto/components/project-card.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
+import Link from "next/link";
+import { FaGithub, FaLink, FaLinkedin } from "react-icons/fa6";
 
 interface ProjectCardProps {
     imgUrl: string;
@@ -15,6 +17,16 @@ const ProjectCard = ({ alt, imgUrl, title }: ProjectCardProps) => {
                     <Image alt={alt} src={imgUrl} fill className="object-cover rounded-xl" />
                 </div>
                 <h3 className="font-bold font-body text-lg">{title}</h3>
+                <div className="flex items-center gap-2 text-sm">
+                    <Link href={"#"}>< FaLink size={24} /></Link>
+                    <h4>Clique para acessar!</h4>
+                </div>
+                <div className="flex items-center justify-end gap-2 text-sm">
+                    <Link href={"#"}><FaGithub size={24} /></Link>
+                    <Link href={"#"}>< FaLinkedin size={24} /></Link>
+                </div>
+
+
 
             </CardContent>
         </Card>
